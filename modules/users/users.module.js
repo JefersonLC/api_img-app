77ff3bc6ac088147a9d209bb3f63fce3e8453383
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-01 19:23:41 -0500

    modified user module

--- modules/users/users.module.js
+++ modules/users/users.module.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import * as UserController from './users.controller.js';
+import { validator } from '../../middlewares/validator.js';
+import { createSchema } from './schemas/create.schema.js';
 
 const userRouter = Router();
 
 userRouter.get('/', UserController.index);
-userRouter.post('/', UserController.create);
+userRouter.post('/', validator(createSchema), UserController.create);
 
 export default userRouter;
