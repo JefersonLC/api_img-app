18d4ae35fdb6c368d32a1066d6cc146e7d27fd90
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-01 19:24:23 -0500

    added midlewares

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import config from './config/environment.js';
 import router from './modules/app.module.js';
+import { logError, showError } from './middlewares/handlerError.js';
 
 const app = express();
 
@@ -10,5 +11,8 @@ app.listen(config.PORT);
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
+
+app.use(logError);
+app.use(showError);
 
 export default app;
