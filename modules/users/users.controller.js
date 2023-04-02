2297ff4a7e7462a186eace4c2004b43a72562cc0
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-01 19:23:26 -0500

    modified user controller

--- modules/users/users.controller.js
+++ modules/users/users.controller.js
@@ -1,15 +1,22 @@
-import sequelize from '../../database/connection.js';
 import UserService from './users.service.js';
 
 const service = new UserService();
 
-export async function index(req, res) {
-  const resp = await sequelize.models.User.findAll();
-  res.json(resp);
+export async function index(req, res, next) {
+  try {
+    const users = await service.findAll();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
 }
 
-export async function create(req, res) {
-  const body = req.body;
-  const resp = await sequelize.models.User.create(body);
-  res.json(resp);
+export async function create(req, res, next) {
+  const data = req.body;
+  try {
+    const users = await service.create(data);
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
 }
