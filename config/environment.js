7498e8aaa6a66f60076a7c061523241f8d029f9e
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-01 19:22:08 -0500

    new environment variable

--- config/environment.js
+++ config/environment.js
@@ -8,7 +8,8 @@ const env = {
   DATABASE_USER: process.env.DATABASE_USER,
   DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
   DATABASE_NAME: process.env.DATABASE_NAME,
-  DATABASE_PORT: Number(process.env.DATABASE_PORT)
+  DATABASE_PORT: Number(process.env.DATABASE_PORT),
+  SECRET: process.env.SECRET
 };
 
 export default env;
