59e085f8d408a6ab4761c6203b5fc2d7d6067e91
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-02 23:05:09 -0500

    modified users controller

--- modules/users/users.controller.js
+++ modules/users/users.controller.js
@@ -1,10 +1,12 @@
 import UserService from './users.service.js';
+import TokenService from '../token/token.service.js';
 
-const service = new UserService();
+const userService = new UserService();
+const tokenService = new TokenService();
 
 export async function index(req, res, next) {
   try {
-    const users = await service.findAll();
+    const users = await userService.findAll();
     res.json(users);
   } catch (error) {
     next(error);
@@ -12,9 +14,17 @@ export async function index(req, res, next) {
 }
 
 export async function create(req, res, next) {
-  const data = req.body;
+  const body = req.body;
+  const payload = {
+    id: body.id,
+    name: body.name,
+    lastname: body.lastname,
+    email: body.email
+  };
+
   try {
-    const users = await service.create(data);
+    const token = tokenService.create(payload);
+    const users = await userService.create(body, token);
     res.json(users);
   } catch (error) {
     next(error);
