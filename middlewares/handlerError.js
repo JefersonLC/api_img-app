52a7c5acfe87b97ed9bc0bd9278ab437bb3819f8
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-03 17:43:49 -0500

    added handler errors

--- middlewares/handlerError.js
+++ middlewares/handlerError.js
@@ -1,8 +1,52 @@
+import { EmptyResultError, UniqueConstraintError } from 'sequelize';
+
 export function logError(err, req, res, next) {
   console.log(err);
   next(err);
 }
 
+export function uniqueConstraintError(err, req, res, next) {
+  if (err instanceof UniqueConstraintError) {
+    return res.status(409).json({
+      name: err.name,
+      errors: err.errors.map((error) => ({
+        message: error.message,
+        type: error.type,
+        value: error.value
+      }))
+    });
+  }
+  next(err);
+}
+
+export function syntaxError(err, req, res, next) {
+  if (err instanceof SyntaxError) {
+    return res.status(400).json(err);
+  }
+  next(err);
+}
+
+export function expectedTokenError(err, req, res, next) {
+  if (err instanceof Error) {
+    return res.status(404).json({
+      name: err.name,
+      message: err.message
+    });
+  }
+  next(err);
+}
+
+export function emptyResultError(err, req, res, next) {
+  if (err instanceof EmptyResultError) {
+    return res.status(404).json({
+      name: err.name,
+      error: err.error,
+      message: err.message
+    });
+  }
+  next(err);
+}
+
 export function showError(err, req, res, next) {
-  res.json(err);
+  return res.json(err);
 }
