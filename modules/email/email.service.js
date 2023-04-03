c014c426913c147914d6db46074eab7100c5b568
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-03 17:46:37 -0500

    email service

---
+++ modules/email/email.service.js
@@ -0,0 +1,30 @@
+import nodemailer from 'nodemailer';
+import env from '../../config/environment.js';
+
+export default class EmailService {
+  async sendEmailtoVerify(token, userEmail) {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: env.EMAIL,
+        pass: env.EMAIL_PASS
+      }
+    });
+
+    const mailOptions = {
+      from: env.EMAIL,
+      to: userEmail,
+      subject: 'Email verification',
+      html: `
+        <div>
+          <p>
+            Verify your email: ${env.DOMAIN}/api/email/verify?token=${token}
+          </p>
+        </div>
+      `
+    };
+    await transporter.sendMail(mailOptions);
+  }
+}
