afd47fc079322cc1057227c8ee72b8c1985333d6
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-03 17:49:11 -0500

    updated create schema

--- modules/users/schemas/create.schema.js
+++ modules/users/schemas/create.schema.js
@@ -1,12 +1,35 @@
+const stringSchema = {
+  type: 'string',
+  minLength: 2,
+  maxLength: 100
+};
+
 export const createSchema = {
   type: 'object',
   properties: {
-    id: { type: 'string', minLength: 2, maxLength: 100 },
-    name: { type: 'string', minLength: 2, maxLength: 100 },
-    lastname: { type: 'string', minLength: 2, maxLength: 100 },
-    age: { type: 'integer', minimum: 14},
-    email: { type: 'string', format: 'email' }
+    id: {
+      allOf: [stringSchema]
+    },
+    name: {
+      allOf: [stringSchema]
+    },
+    lastname: {
+      allOf: [stringSchema]
+    },
+    age: {
+      type: 'integer',
+      minimum: 14
+    },
+    email: {
+      type: 'string',
+      format: 'email'
+    },
+    password: {
+      type: 'string',
+      minLength: 8,
+      maxLength: 24
+    }
   },
-  required: ['id', 'name', 'lastname', 'age', 'email'],
+  required: ['id', 'name', 'lastname', 'age', 'email', 'password'],
   additionalProperties: false
 };
