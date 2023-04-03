1644831887cd7fe76a3cd44ce7bf64403c05e43b
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-03 17:49:42 -0500

    added /email route

--- modules/app.module.js
+++ modules/app.module.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import userRouter from './users/users.module.js';
+import emailRouter from './email/email.module.js';
 
 const router = Router();
 
 router.use('/users', userRouter);
+router.use('/email', emailRouter);
 
 export default router;
