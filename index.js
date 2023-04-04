6f7958a72fd4e96fd9ec46f139b3872d409fb519
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-04 18:10:32 -0500

    added passport strategies

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import passport from 'passport';
 import config from './config/environment.js';
 import router from './modules/app.module.js';
-import {
-  emptyResultError,
-  expectedTokenError,
-  logError,
-  showError,
-  syntaxError,
-  uniqueConstraintError
-} from './middlewares/handlerError.js';
+import * as Handler from './middlewares/handlerError.js';
+import { localStrategy } from './middlewares/passport/local.strategy.js';
+import { jwtStrategy } from './middlewares/passport/jwt.strategy.js';
 
 const app = express();
 
@@ -17,13 +13,22 @@ app.listen(config.PORT);
 
 app.use(cors());
 app.use(express.json());
+app.use(passport.initialize());
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
 app.use('/api', router);
 
-app.use(logError);
-app.use(uniqueConstraintError);
-app.use(syntaxError);
-app.use(expectedTokenError);
-app.use(emptyResultError);
-app.use(showError);
+app.get('/verifyuser', (req, res) => {
+  res.json({ user: req.user || null});
+});
+
+app.use(Handler.logError);
+app.use(Handler.uniqueConstraintError);
+app.use(Handler.syntaxError);
+app.use(Handler.expectedTokenError);
+app.use(Handler.emptyResultError);
+app.use(Handler.showError);
 
 export default app;
