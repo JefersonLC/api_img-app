372ec3f1a9e3ceaba00b353b5073b98733b03702
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-04 18:06:19 -0500

    updated app module

--- modules/app.module.js
+++ modules/app.module.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import loginRouter from './login/login.module.js';
 import userRouter from './users/users.module.js';
 import emailRouter from './email/email.module.js';
 
 const router = Router();
 
+router.use('/login', loginRouter);
 router.use('/users', userRouter);
 router.use('/email', emailRouter);
 
