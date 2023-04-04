c08d29079817b0f51f24f9b46a9864a6561eb3ed
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-04 18:09:33 -0500

    updated users module

--- modules/users/users.module.js
+++ modules/users/users.module.js
@@ -2,10 +2,28 @@ import { Router } from 'express';
 import * as UserController from './users.controller.js';
 import { validator } from '../../middlewares/validator.js';
 import { createSchema } from './schemas/create.schema.js';
+import passport from 'passport';
 
 const userRouter = Router();
 
-userRouter.get('/', UserController.index);
+userRouter.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  UserController.index
+);
+
 userRouter.post('/', validator(createSchema), UserController.create);
+
+userRouter.get(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  UserController.getUser
+);
+
+userRouter.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  UserController.remove
+);
 
 export default userRouter;
