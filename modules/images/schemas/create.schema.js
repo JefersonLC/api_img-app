bcb04eed4fa9034c5e6af00eb60701cbcc3ccb79
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-10 21:30:31 -0500

    new scheme to add images

--- modules/images/schemas/create.schema.js
+++ modules/images/schemas/create.schema.js
@@ -1,1 +1,12 @@
-export const createSchema = {};
+export const createSchema = {
+  type: 'object',
+  properties: {
+    description: {
+      type: 'string',
+      minLength: 2,
+      maxLength: 150
+    }
+  },
+  required: ['description'],
+  additionalProperties: false
+};
