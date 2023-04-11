633f3b53b59446f183064a3d8f3f034e55ff594a
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-10 21:35:05 -0500

    new path /images

--- modules/app.module.js
+++ modules/app.module.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import loginRouter from './login/login.module.js';
 import userRouter from './users/users.module.js';
 import emailRouter from './email/email.module.js';
+import imageRouter from './images/images.module.js';
 
 const router = Router();
 
 router.use('/login', loginRouter);
 router.use('/users', userRouter);
 router.use('/email', emailRouter);
+router.use('/images', imageRouter);
 
 export default router;
