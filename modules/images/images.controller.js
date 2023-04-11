28f3f98765775ac3d3ba1d50c71cc9da26087ab8
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-10 21:32:58 -0500

    new image controller

--- modules/images/images.controller.js
+++ modules/images/images.controller.js
@@ -1,3 +1,25 @@
-export function index(req, res, next) {}
+import ImageService from './images.service.js';
 
-export function create(req, res, next) {}
+const imageService = new ImageService();
+
+export async function index(req, res, next) {
+  try {
+    const images = await imageService.findAll();
+    res.json(images);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function create(req, res, next) {
+  const body = req.body;
+  const image = req.file;
+  const { id } = req.user;
+
+  try {
+    const newImage = await imageService.create({ image, body, id });
+    res.status(201).json(newImage);
+  } catch (error) {
+    next(error);
+  }
+}
