ee23b7ecdb3762495df01bd06fdd4c47e5d5b246
JefersonLC <c.jeferson.lc@gmail.com>
2023-04-10 21:25:03 -0500

    updated index.js

--- index.js
+++ index.js
@@ -4,23 +4,25 @@ import passport from 'passport';
 import env from './config/environment.js';
 import router from './modules/app.module.js';
 import * as Handler from './middlewares/handlerError.js';
-import { localStrategy } from './middlewares/passport/local.strategy.js';
-import { jwtStrategy } from './middlewares/passport/jwt.strategy.js';
+import { jwtStrategy, localStrategy } from './config/auth.js';
 
 const app = express();
 
 app.listen(env.PORT);
 
 app.use(cors());
+app.use('/public', express.static('public'));
 app.use(express.json());
+
 app.use(passport.initialize());
 
-passport.use(localStrategy);
 passport.use(jwtStrategy);
+passport.use(localStrategy);
 
 app.use('/api', router);
 
 app.use(Handler.logError);
+app.use(Handler.multerError);
 app.use(Handler.uniqueConstraintError);
 app.use(Handler.syntaxError);
 app.use(Handler.boomError);
